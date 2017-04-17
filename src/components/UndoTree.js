6e6e4b3da1c6b10e4dc058259fbd415b6c70cb72
lxgreen <alxgreen@gmail.com>
2017-04-18 02:30:24 +0300

    react-tree-graph added for tree visualization; sample use is added

--- src/components/UndoTree.js
+++ src/components/UndoTree.js
@@ -1,12 +1,53 @@
 import React, { Component } from 'react';
+import Tree from 'react-tree-graph';
 
+import 'react-tree-graph/dist/style.css';
 import '../../css/components/UndoTree.css';
 
 class UndoTree extends Component {
+    constructor() {
+        super();
+        this.state = { 
+            data: {
+                'name': 'Eve',
+                'children': [
+                    {
+                        'name': 'Cain'
+                    },
+                    {
+                        'name': 'Seth',
+                        'children': [
+                            {
+                                'name': 'Enos'
+                            },
+                            {
+                                'name': 'Noam'
+                            }
+                        ]
+                    },
+                    {
+                        'name': 'Abel'
+                    },
+                    {
+                        'name': 'Awan',
+                        'children': [
+                            {
+                                'name': 'Enoch'
+                            }
+                        ]
+                    },
+                    {
+                        'name': 'Azura'
+                    }
+                ]
+            } 
+        };
+    }
+
     render() {
         return (
-            <div className="UndoTree">
-                <div className="state init">new</div>
+            <div className='UndoTree'>
+            <Tree data={this.state.data} height={400} width={400} />
             </div>
         );
     }
